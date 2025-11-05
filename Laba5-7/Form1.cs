875bf0bff70865b75866d327142fb43c2c9910f6
Atrem <a.shipalkin@ya.ru>
2025-11-05 21:27:58 +0300

    Добавил алгоритмы неоптимального и оптимального бинарного поиска

--- Laba5-7/Form1.cs
+++ Laba5-7/Form1.cs
@@ -29,7 +29,66 @@ namespace Laba5_7
 
         private void FindButton_Click(object sender, EventArgs e)
         {
-            
+            int index = -1;
+            int key = (int)Key.Value;
+
+            //Неоптимальный бинарный поиск
+            int StartTime = Environment.TickCount;
+            for (int j = 0; j < CountOfFind; j++)
+            {
+                int l = 0;
+                int r = n - 1;
+
+                while (r >= l)
+                {
+                    int i = (r + l) / 2;
+                    if (key == mas[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                    if (key < mas[i])
+                    {
+                        r = i - 1;
+                    }
+                    else
+                    {
+                        l = i + 1;
+                    }
+                }
+            }
+            int ResultTime = Environment.TickCount - StartTime;
+
+            Time01.Text = ResultTime.ToString();
+            Index01.Text = (index >= 0) ? index.ToString() : "поиск неудачен";
+
+            //Оптимальный бинарный поиск
+            index = -1;
+
+            int StartTime1 = Environment.TickCount;
+            for (int j = 0; j < CountOfFind; j++)
+            {
+                int l = 0;
+                int r = n - 1;
+
+                while (r > l)
+                {
+                    int i = (r + l) / 2;
+                    if (key <= mas[i])
+                    {
+                        r = i;
+                    }
+                    else
+                    {
+                        l = i + 1;
+                    }
+                }
+                index = (mas[r] == key) ? r : -1;
+            }
+            int ResultTime1 = Environment.TickCount - StartTime1;
+
+            Time02.Text = ResultTime1.ToString();
+            Index02.Text = (index >= 0) ? index.ToString() : "поиск неудачен";
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
